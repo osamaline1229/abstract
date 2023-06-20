893557507ed88e41db97186a651e3b82b659b116
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-20 01:35:36 -0700

    virtual

---
+++ Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheiratance
+{
+    internal abstract class Animal
+    {
+        public string Name;
+        public int Age;
+
+        public Animal(string name, int age)
+        {
+           this.Name = name;
+            this.Age = age;
+            
+        }
+
+        public abstract void makeSound();
+
+        public void printSomething()
+        {
+            Console.WriteLine("printing something");
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Animal animal2 = obj as Animal;
+            if (animal2 == null){ return false; }
+            return this.Name == ((Animal)obj).Name;
+        }
+
+    }
+
+    internal class Dog : Animal
+    {
+        string Bread;
+        string Sound;
+
+        public Dog(string name, int age, string Bread, string Sound) : base(name, age)
+        {
+            this.Bread = Bread;
+            this.Sound = Sound;
+        }
+
+        public void getAnimalInfo()
+        {
+            Console.WriteLine(this.Name+ "" + this.Age + "" + Bread+ "" + Sound);
+        }
+
+        public override void makeSound()
+        {
+            Console.WriteLine("sound of Dog");
+        }
+
+        public void abc()
+        {
+            //functionality
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using inheiratance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Dog d = new Dog("Luna" , 4, "Havenese", "Brrr!!!");
+        //Dog d2 = new Dog("Luna" , 4, "Havenese", "Brrr!!!");
+        //Console.WriteLine(d.Equals(d2));
+        //d.doSomething();
+        //d.makeSoung();
+        //string abc = d.ToString();
+        //Console.WriteLine(d);
+        //Console.WriteLine("--------------");
+        //Cat cat = new Cat("Bruno", 22);
+        // cat.doSomething();
+        //cat.makeSound();
+        //cat.printSomething();
+
+        Animal a = new Dog("Luna", 4, "Havenese", "Brrr!!!");
+        a.makeSound();
+        Console.WriteLine("Hello, World!");
+    }
+}
